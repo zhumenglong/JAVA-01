8b140bf3e7f3180590e93b1f9b281aef8175240e
zhumenglong <779495884@qq.com>
2021-01-26 23:41:27 +0800

    NettyHttpClient

--- Week_03/src/main/java/gateway/outbound/netty4/NettyHttpClient.java
+++ Week_03/src/main/java/gateway/outbound/netty4/NettyHttpClient.java
@@ -2,14 +2,21 @@ package gateway.outbound.netty4;
 
 import gateway.router.HttpEndpointRouter;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.FullHttpRequest;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
 
 @Slf4j
 public class NettyHttpClient {
@@ -27,38 +34,47 @@ public class NettyHttpClient {
 
     public NettyHttpClient(List<String> urls) {
         this.urls = urls;
+        this.channelFutureMap = new HashMap<>(urls.size());
     }
 
     public void start() {
         log.info("开始启动客户端");
+        Bootstrap bootstrap = new Bootstrap();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            Bootstrap bootstrap = new Bootstrap();
 
-
-            ChannelFuture channelFuture = bootstrap.group(workerGroup)
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.SO_KEEPALIVE, true)
-                    .handler(new NettyHttpClientOutboundHandler() {
-                    })
-                    .connect("RpcConstant.ADDRESS", 1111)
-                    .syncUninterruptibly();
-
-            channelFutureMap.put("ip",channelFuture);
-            log.info("RPC 服务启动客户端完成，监听端口：" + 1111);
-        } catch (Exception e) {
-            log.error("RPC 客户端遇到异常", e);
-            throw new RuntimeException(e);
+        bootstrap.group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new NettyHttpClientOutboundHandler() {
+                });
+        for (int i = 0; i < urls.size(); i++) {
+            Pattern p = compile("(\\d+\\.\\d+\\.\\d+\\.\\d+)\\:(\\d+)");
+            Matcher m = p.matcher(urls.get(i));
+            String host = null;
+            String port = null;
+            while (m.find()) {
+                host = m.group(1);
+                port = m.group(2);
+            }
+            try {
+                ChannelFuture channelFuture = bootstrap.connect(host, Integer.parseInt(port)).syncUninterruptibly();
+                channelFutureMap.put(urls.get(i), channelFuture);
+                log.info("RPC 服务启动客户端完成，监听端口：" + port);
+            } catch (Exception e) {
+                log.error("RPC 客户端遇到异常", e);
+                throw new RuntimeException(e);
+            }
         }
     }
 
 
-    public void fetchGet(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final String url) {
-
-        String ip = url.split(":")[0];
-        ChannelFuture channelFuture = channelFutureMap.get(ip);
+    public void execute(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final String url) {
+        ChannelFuture channelFuture = channelFutureMap.get(url);
         Channel channel = channelFuture.channel();
         log.info("客户端发送请求，url: {}", url);
-
+        String msg = "{\"msgType\":\"req\",\"clientId\":\"请求数据\"}";
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeBytes(msg.getBytes());
+        channel.writeAndFlush(buf);
     }
 }
--- Week_03/src/main/java/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
+++ Week_03/src/main/java/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
@@ -1,19 +1,69 @@
 package gateway.outbound.netty4;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import lombok.extern.slf4j.Slf4j;
 
+
+import java.net.InetSocketAddress;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+@Slf4j
+@ChannelHandler.Sharable
 public class NettyHttpClientOutboundHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-
-
+        InetSocketAddress ipSocket = (InetSocketAddress) ctx.channel().remoteAddress();
+        int port = ipSocket.getPort();
+        String host = ipSocket.getHostString();
+        log.info("与" + host + ":" + port + "连接成功!");
+        ctx.fireChannelActive();
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        InetSocketAddress ipSocket = (InetSocketAddress) ctx.channel().remoteAddress();
+        int port = ipSocket.getPort();
+        String host = ipSocket.getHostString();
+        ByteBuf byteBuf = (ByteBuf) msg;
+        handleResponse( ctx, byteBuf);
 
+    }
 
+    private void handleResponse(final ChannelHandlerContext ctx, final ByteBuf byteBuf) throws Exception {
+        FullHttpResponse response = null;
+        try {
+            byte[] body = new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(body);
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            ctx.write(response);
+            ctx.flush();
+            //ctx.close();
+        }
+
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
