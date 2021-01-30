ce3947f22a83e85f253d8cf91001a6c17faa8335
zhumenglong <779495884@qq.com>
2021-01-30 23:43:10 +0800

    Homework03

--- Week_04/src/main/java/java0/conc0303/Homework03.java
+++ Week_04/src/main/java/java0/conc0303/Homework03.java
@@ -3,12 +3,13 @@ package java0.conc0303;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
  * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
  * 写出你的方法，越多越好，提交到github。
- *
+ * <p>
  * 一个简单的代码参考：
  */
 public class Homework03 {
@@ -16,26 +17,43 @@ public class Homework03 {
     public static void main(String[] args) {
 
         /**
-         * 方式1
+         * 方式0: join
+         */
+        AtomicInteger result0 = new AtomicInteger();
+        Thread thread = new Thread(() -> {
+            result0.set(sum());
+        });
+        try {
+            thread.start();
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result0.get());
+
+
+        /**
+         * 方式1: while
          */
         AtomicBoolean resultFlag = new AtomicBoolean(false);
         AtomicInteger result1 = new AtomicInteger();
-        new Thread(()->{
+        new Thread(() -> {
             result1.set(sum());
             resultFlag.set(true);
         }).start();
 
-        while (true){
-            if(resultFlag.get()){
+        while (true) {
+            if (resultFlag.get()) {
                 break;
             }
         }
         System.out.println(result1);
 
 
-        Callable<Integer> callable = ()->sum();
+        Callable<Integer> callable = () -> sum();
+
         /**
-         * 方式2
+         * 方式2: FutureTask
          */
         FutureTask<Integer> futureTask = new FutureTask<>(callable);
         new Thread(futureTask).start();
@@ -49,7 +67,7 @@ public class Homework03 {
         }
 
         /**
-         * 方式3
+         * 方式3: FutureTask + ExecutorService
          */
         ExecutorService executorService = Executors.newCachedThreadPool();
         Future<Integer> submit = executorService.submit(callable);
@@ -60,13 +78,93 @@ public class Homework03 {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
+        } finally {
+            executorService.shutdown();
         }
 
         /**
-         * 方式4
+         * 方式4: CompletableFuture
          */
         Integer result4 = CompletableFuture.supplyAsync(() -> sum()).join();
         System.out.println(result4);
+
+        /**
+         * 方式5: Lock
+         */
+        Object lock = new Object();
+        AtomicInteger result5 = new AtomicInteger();
+        Thread t5 = new Thread(() -> {
+            result5.set(sum());
+            synchronized (lock) {
+                lock.notify();//子线程唤醒
+            }
+        });
+        t5.start();
+        try {
+            synchronized (lock) {
+                lock.wait();//主线程等待
+            }
+            System.out.println(result5.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * 方式6：CountDownLatch
+         */
+        CountDownLatch cdl = new CountDownLatch(1);
+        AtomicInteger result6 = new AtomicInteger();
+        new Thread(() -> {
+            result6.set(sum());
+            cdl.countDown();
+        }).start();
+
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result6.get());
+
+        /**
+         * 方式7：BlockingQueue
+         */
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue(1);//数组型队列，长度为1
+        new Thread(() -> {
+            queue.add(sum());
+        }).start();
+        try {
+            Integer take = queue.take();
+            System.out.println(take);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * 方式8：CyclicBarrier
+         */
+        CyclicBarrier cb = new CyclicBarrier(2);
+        AtomicInteger result8 = new AtomicInteger();
+        new Thread(() -> {
+            try {
+                result8.set(sum());
+                cb.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        try {
+            cb.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(result8.get());
+
+
 
     }
 
